92a7e49e6e43cfbd0c8a803d4d960f171b3a7b97
Liam Walker <robert.liam.walker@gmail.com>
2024-06-13 22:17:14 -0500

    UI Change for workouts view and adding a new workout

--- Coolcicada PPL/Views/WorkoutView.swift
+++ Coolcicada PPL/Views/WorkoutView.swift
@@ -9,28 +9,55 @@ struct WorkoutView: View {
     
     var body: some View {
         NavigationView {
-            List {
-                ForEach(viewModel.workouts) { workout in
-                    HStack {
-                        NavigationLink(destination: WorkoutDetailsView(viewModel: viewModel, workout: workout)) {
-                            Text(workout.name)
+            VStack {
+                List {
+                    ForEach(viewModel.workouts) { workout in
+                        HStack {
+                            NavigationLink(destination: WorkoutDetailsView(viewModel: viewModel, workout: workout)) {
+                                Text(workout.name)
+                            }
+                            Spacer()
+                            Button(action: {
+                                viewModel.deleteWorkout(workout)
+                            }) {
+                                Image(systemName: "xmark")
+                                    .font(.system(size: 14))
+                                    .foregroundColor(.blue)
+                            }
+                            .buttonStyle(PlainButtonStyle())
                         }
-                        Spacer()
-                        Button(action: {
-                            viewModel.deleteWorkout(workout)
-                        }) {
-                            Image(systemName: "xmark")
-                                .font(.system(size: 14)) // Adjust the font size here
-                                .foregroundColor(.blue)
-                        }
-                        .buttonStyle(PlainButtonStyle())
                     }
                 }
-                NavigationLink(destination: CustomWorkoutView(viewModel: viewModel.customWorkoutViewModel)) {
-                    Text("Add Custom Workout")
+                .listStyle(PlainListStyle())
+                .navigationBarTitle("Templates")
+                
+                Spacer()
+                
+                HStack {
+                    NavigationLink(destination: CustomWorkoutView(viewModel: viewModel.customWorkoutViewModel)) {
+                        Text("Add Template")
+                            .font(.subheadline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                            .padding([.leading, .bottom])
+                    }
+                    
+                    NavigationLink(destination: Text("Quick Start Workout View")) { // Placeholder for Quick Start Workout View
+                        Text("Quick Start")
+                            .font(.subheadline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                            .padding([.trailing, .bottom])
+                    }
                 }
+                .padding([.horizontal])
             }
-            .navigationBarTitle("Workouts")
         }
     }
 }
